da59881bb69805126697381ed1ce664281b5dcb5
Jerad Acosta <jsacosta1103@gmail.com>
2014-01-11 14:00:04 -1000

    BNR Learning Exercises
    Challenges as the end of each chapter in Big Nerd Ranch Obj-C
    programming

---
+++ Obj-C_Big_Nerd_Ranch_Guide/iTahDoodleMBA/iTahDoodleMBA/BNRAppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  BNRAppDelegate.h
+//  iDO
+//
+//  Created by Jerad Acosta on 12/14/13.
+//  Copyright (c) 2013 smAsh Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// Declare a helper function that we will use to get a path
+// to the location on desk where we can save the to-do list
+NSString *docPath(void);
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate, UITableViewDataSource>
+{
+    UITableView *taskTable;
+    UITextField *taskField;
+    UIButton *insertButton;
+    
+    NSMutableArray *tasks;
+}
+
+- (void)addTask:(id)sender;
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
